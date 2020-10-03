3dd78e3fe44df556854d21d6768d81e1d8d5a987
Jan Stolarek <jan.stolarek@ed.ac.uk>
2020-10-03 18:27:40 +0100

    Fix compilation errors in Direction

--- src/common/Direction.hx
+++ src/common/Direction.hx
@@ -1,3 +1,5 @@
+package common;
+
 enum abstract Direction(Int) from Int to Int {
   var UP         = 0;
   var UP_RIGHT   = 1;
@@ -10,38 +12,29 @@ enum abstract Direction(Int) from Int to Int {
 
   public static inline var length : Int = 8;
 
-  public static directionTo( fromX : Float, fromY : Float
-                           , toX   : Float, toY   : Float ) {
+  public static function directionTo( fromX : Float, fromY : Float
+                                    , toX   : Float, toY   : Float ) {
     var direction = UP;
     var angle     = Utils.angleTo( fromX, fromY, toX, toY );
 
-    if ( angle > -M.PI/8 && angle <= M.PI/8 ) {
+    if ( angle > -Math.PI/8 && angle <= Math.PI/8 ) {
       direction = RIGHT;
-    } else if ( angle < -1/8*M.PI && angle >= -3/8*M.PI ) {
+    } else if ( angle < -1/8 * Math.PI && angle >= -3/8 * Math.PI ) {
       direction = UP_RIGHT;
-    } else if ( angle < -3/8*M.PI && angle >= -5/8*M.PI ) {
+    } else if ( angle < -3/8 * Math.PI && angle >= -5/8 * Math.PI ) {
       direction = UP;
-    } else if ( angle < -5/8*M.PI && angle >= -7/8*M.PI ) {
+    } else if ( angle < -5/8 * Math.PI && angle >= -7/8 * Math.PI ) {
       direction = UP_LEFT;
-    } else if ( angle < -7/8*M.PI ) {
+    } else if ( angle < -7/8 * Math.PI ) {
       direction = LEFT;
-    } else if ( angle > 1/8*M.PI && angle <= 3/8*M.PI ) {
+    } else if ( angle > 1/8 * Math.PI && angle <= 3/8 * Math.PI ) {
       direction = DOWN_RIGHT;
-    } else if ( angle > 3/8*M.PI && angle <= 5/8*M.PI ) {
+    } else if ( angle > 3/8 * Math.PI && angle <= 5/8 * Math.PI ) {
       direction = DOWN;
-    } else if ( angle > 5/8*M.PI && angle <= 7/8*M.PI ) {
+    } else if ( angle > 5/8 * Math.PI && angle <= 7/8 * Math.PI ) {
       direction = DOWN_LEFT;
     }
 
     return direction;
-  }
-
-  // Use to flip animation when player facing left
-  public static inline flipDirection( d : Direction ) : Int {
-    var flip = 1;
-    if ( d == UP_LEFT || d == LEFT || d == DOWN_LEFT ) {
-      flip = -1;
-    }
-    return flip;
   }
 }
