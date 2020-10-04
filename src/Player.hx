52faeee6b260b77bc2eebe8b22b2c7caef55e445
Jan Stolarek <jan.stolarek@ed.ac.uk>
2020-10-04 21:24:52 +0100

    Least bad collisions so far

--- src/Player.hx
+++ src/Player.hx
@@ -105,6 +105,19 @@ class Player extends Entity<State, String> {
 
   var collisionBox      : Rect;
 
+  var left    (get, never) : Float; inline function get_left()    { return x    + collisionBox.x;         }
+  var up      (get, never) : Float; inline function get_up()      { return y    + collisionBox.y;         }
+  var right   (get, never) : Float; inline function get_right()   { return left + collisionBox.w;         }
+  var down    (get, never) : Float; inline function get_down()    { return up   + collisionBox.h;         }
+  var leftCx  (get, never) : Int;   inline function get_leftCx()  { return Math.floor( left  / gx );      }
+  var upCx    (get, never) : Int;   inline function get_upCx()    { return Math.floor( up    / gy );      }
+  var rightCx (get, never) : Int;   inline function get_rightCx() { return Math.floor( right / gx );      }
+  var downCx  (get, never) : Int;   inline function get_downCx()  { return Math.floor( down  / gy );      }
+  var leftXr  (get, never) : Float; inline function get_leftXr()  { return (left  - (leftCx  * gx)) / gx; }
+  var upYr    (get, never) : Float; inline function get_upYr()    { return (up    - (upCx    * gy)) / gy; }
+  var rightXr (get, never) : Float; inline function get_rightXr() { return (right - (rightCx * gx)) / gx; }
+  var downYr  (get, never) : Float; inline function get_downYr()  { return (down  - (downCx  * gy)) / gy; }
+
   // Properties for convenience
   var console (get, never) : Console;
 
@@ -118,9 +131,10 @@ class Player extends Entity<State, String> {
     controller = Controller.getController( );
     actions    = new Vector<Bool>( Action.length );
     unnoticed = true;
-
-    cx = 6;
-    cy = 8;
+    // starting coordinates
+    cx = 12;
+    cy = 1;
+    yr = 0.25;
 
     collisionBox = { x : 6, y : 24, w : 19, h : 7 };
 
@@ -138,6 +152,8 @@ class Player extends Entity<State, String> {
   }
 
   override function fixedUpdate( ) {
+    vx = vy = 0.0; // either this of applySpeedFriction below
+
     if ( isAction( UP ) ) {
       vy = -Const.PLAYER_SPEED;
     }
@@ -156,21 +172,18 @@ class Player extends Entity<State, String> {
 
     setDebugLabel( "(x=" + Utils.floatToString( cx + xr, 2 ) +
                   ", y=" + Utils.floatToString( cy + yr, 2 ) + ")", 0x66dd99 );
-    // END TEST STUFF
-
 
     xr += vx;
     yr += vy;
     handleCollisions( );
 
-    applySpeedFriction( );
+//    applySpeedFriction( );
   }
 
   private inline function setAnimations() : Void {
-    // BEGIN TEST STUFF
     animation.stateAnims =
       Aseprite.loadStateAnimation( "player", State.fromString );
-//    animation.pivot = new Animation.Pivot( 0.5, 1, true );
+//    animation.pivot = new Animation.Pivot( 0.5, 0.5, true );
 
     animation.registerStateAnimation( WALK_UP, 1, function ( ) {
       return actions[ UP ];
@@ -215,52 +228,42 @@ class Player extends Entity<State, String> {
   }
 
   private inline function handleCollisions( ) : Void {
-    var left  = x    + collisionBox.x;
-    var up    = y    + collisionBox.y;
-    var right = left + collisionBox.w;
-    var down  = up   + collisionBox.h;
-
-    var leftCx  : Int = Math.floor( left  / gx );
-    var upCx    : Int = Math.floor( up    / gy );
-    var rightCx : Int = Math.floor( right / gx );
-    var downCx  : Int = Math.floor( down  / gy );
-
-    var leftXr  = (left  - (leftCx  * gx)) / gx;
-    var upYr    = (up    - (upCx    * gy)) / gy;
-    var rightXr = (right - (rightCx * gx)) / gx;
-    var downYr  = (down  - (downCx  * gy)) / gy;
-
     // if moving up then check collisions of both upper corners
     if ( vy < 0 && ( level.hasDeskCollision( leftCx , upCx ) ||
                      level.hasDeskCollision( rightCx, upCx ) ) ) {
+      yr -= vy;
       vy = 0.0; // stop
-//      cy = upCx;
-//      yr = 0.25;
+      //cy = upCx;
+      //yr = 0.25;
     }
 
     // if moving down check collisions on both lower corners
     if ( vy > 0 && ( level.hasDeskCollision( leftCx , downCx ) ||
                      level.hasDeskCollision( rightCx, downCx ) ) ) {
+      yr -= vy;
       vy = 0.0; // stop
-      cy = downCx - 1;
-      yr = 0.0;
+//      cy = downCx - 1;
+//      yr = 0.0;
     }
 
     // if moving left check collisions on both left corners
     if ( vx < 0 && ( level.hasDeskCollision( leftCx, downCx ) ||
                      level.hasDeskCollision( leftCx, upCx   ) ) ) {
+      xr -= vx;
       vx = 0.0; // stop
-      cx = leftCx;
-      xr = 0.85;
+//      cx = leftCx;
+//      xr = 0.85;
     }
 
     // if moving right check collisions on both right corners
     if ( vx > 0 && ( level.hasDeskCollision( rightCx, downCx ) ||
                      level.hasDeskCollision( rightCx, upCx   ) ) ) {
+      xr -= vx;
       vx = 0.0; // stop
-      cx = rightCx - 1;
-      xr = 0.2;
+//      cx = rightCx - 1;
+//      xr = 0.2;
     }
+
   }
 
   private inline function resetActions( ) : Void {
@@ -283,6 +286,16 @@ class Player extends Entity<State, String> {
       actions[ ATTACK ] = isKeyDown( keyInputs.attack, padInputs.attack );
     } else {
       resetActions( );
+    }
+
+    if ( actions[ UP ] && actions[ DOWN ] ) {
+      actions[ UP   ] = false;
+      actions[ DOWN ] = false;
+    }
+
+    if ( actions[ LEFT ] && actions[ RIGHT ] ) {
+      actions[ LEFT  ] = false;
+      actions[ RIGHT ] = false;
     }
 
     super.update( );
